75616bdf8cf95db2ce9fcdf30a83dc9a57e19a01
FREITASANDRE <36485479+FREITASANDRE@users.noreply.github.com>
2020-03-14 11:11:13 -0300

    Add files via upload

---
+++ backend/Database/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App_Api.Models
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
