e07b10dad65e9c27b1cb06d80942df87257ec6c9
FREITASANDRE <36485479+FREITASANDRE@users.noreply.github.com>
2020-03-14 11:10:54 -0300

    Update

--- backend/Database/User.cs
+++ backend/Database/User.cs
@@ -1,1 +1,23 @@
+using System;
 
+namespace App_Api.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public decimal Password { get; set; }
+        public DateTime Created
+        {
+            get
+            {
+                return this.Created;
+            }
+            set
+            {
+                value = DateTime.Now;
+            }
+        }
+    }
+}
