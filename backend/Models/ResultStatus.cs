a65e1992332d33582ef908a7e227d64874882bc1
FREITASANDRE <36485479+FREITASANDRE@users.noreply.github.com>
2020-03-14 11:13:28 -0300

    Criado

---
+++ backend/Models/ResultStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Api.Models
+{
+    public enum ResultStatus
+    {
+        Success = 0,
+        Error = 1
+    }
+}
---
+++ backend/Models/UserBusiness.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Api.Models
+{
+    public class UserBusiness
+    {
+        private readonly AppContext _context;
+
+        public UserBusiness(AppContext pContext)
+        {
+            _context = pContext;
+        }
+
+        public async Task<Response<List<User>>> GetAll()
+        {
+            try
+            {
+                var lUsers = await _context.Users.ToListAsync();
+
+                if (lUsers != null)
+                {
+                    return new Response<List<User>>()
+                    {
+                        Message = BaseMessages.SuccessGet,
+                        Status = ResultStatus.Success,
+                        Result = lUsers
+                    };
+                }
+
+                return new Response<List<User>>()
+                {
+                    Message = BaseMessages.NotFoundGet,
+                    Status = ResultStatus.Success,
+                    Result = null
+                };
+            }
+            catch (Exception lEx)
+            {
+                return new Response<List<User>>()
+                {
+                    Message = lEx.Message,
+                    Status = ResultStatus.Error,
+                    Result = null
+                };
+            }
+        }
+
+    }
+}
