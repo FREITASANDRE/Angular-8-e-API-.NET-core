35f7d0c4c3328dd6cc20b341a459e33861c023a8
Andre Freitas <36485479+FREITASANDRE@users.noreply.github.com>
2020-03-20 10:43:39 -0300

    Update app.service.ts

--- frontend/src/app/service/app.service.ts
+++ frontend/src/app/service/app.service.ts
@@ -1,1 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppConstants } from '../app-constants';
 
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+
+  async SendToApi(Path, Data) {
+    //implementar chamada para buscar na api e validar os dados do usuário
+    console.log(AppConstants.baseServer + Path);
+    /*return await this.http.post(AppConstants.baseServer + Path, JSON.stringify(Data)).subscribe(data => {
+        data
+  }*/
+   
+  }
+}
